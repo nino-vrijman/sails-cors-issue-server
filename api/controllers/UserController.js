75eeb7e291ed949eb9e4b81f0fef52c8b2fe0dfa
Nino Vrijman <ninovrijman@gmail.com>
2020-05-03 11:55:56 +0200

    initial commit

---
+++ api/controllers/UserController.js
@@ -0,0 +1,12 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  test: function (req, res) {
+    return res.send('ok');
+  },
+};
